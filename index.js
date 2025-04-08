b75dba00f2bf7591c2918094515ece91db0ade89
gojo581 <gadi17levin@gmail.com>
2025-04-08 15:21:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function addpoints(){
 
 point=point+100
 coin.innerHTML=point
-if(point==30000){alert('finally... you did it! you won all the game!!!!!!')||window.location.replace('file:///C:/Users/gadi1/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/catch%20game%20finish/index.html')}
+if(point==30000){alert('finally... you did it! you won all the game!!!!!!')||window.location.replace('https://gojo581.github.io/catch-game-finish/')}
 }
