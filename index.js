8f7333e2111e442cc940be279d0f5f41769db337
gojo581 <gadi17levin@gmail.com>
2025-04-08 15:09:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let speed=2
+let point=document.getElementById('coin')
+function addpoints(){
+
+point=point+100
+coin.innerHTML=point
+if(point==30000){alert('finally... you did it! you won all the game!!!!!!')||window.location.replace('file:///C:/Users/gadi1/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/catch%20game%20finish/index.html')}
+}
